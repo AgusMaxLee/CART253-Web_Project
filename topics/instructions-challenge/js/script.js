920e76bed7f4e3b144c8fc63c129a0024e4ef455
AgusMaxLee <117362297+AgusMaxLee@users.noreply.github.com>
2026-09-17 15:39:24 -0400

    Added head, eye and goggles

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -1,27 +1,102 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Minion
+ * Name
+ *
+ * It is a minion.
  */
 
-"use strict";
-
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas
+ */
 function setup() {
   createCanvas(400, 400);
 }
 
+/**
+ * Draws the cat
+ */
+function draw() {
+  // Pink background
+  background(255, 180, 180);
+  
+  // No stroke everywhere!
+  noStroke();
+  
+  drawCat();
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Draws the cat using functions
+ */
+function drawCat() {
+  drawHead();
+  drawEyes();
+//   drawNose();
+//   drawMouth();
+}
 
-    
-    
+/**
+ * Draws the cat's head (including its ears)
+ */
+function drawHead() {
+  
+  // Minion head
+
+  push();
+  fill("yellow");
+  circle(200, 200, 250);
+  
+  // Minion body
+
+  fill("yellow");
+  rect(75, 200, 250, 200);
+  pop();
+}
+
+/**
+ * Draws the minion's eye
+ */
+function drawEyes() {
+     // goggles
+  push();
+  fill("black");
+  rect(70, 170, 260, 40);
+  pop();
+    // goggles
+  push();
+  fill("#97978f");
+  ellipse(200, 200, 160);
+  pop();
+    // eye
+  push();
+  fill(255);
+  ellipse(200, 200, 120);
+  fill(0);
+  ellipse(200, 200, 50);
+  pop();
 
 }
+
+// /**
+//  * Draws the cat's nose
+//  */
+// function drawNose() {
+//   // Cat nose
+//   push();
+//   fill(255, 100, 100);
+//   triangle(190, 340, 210, 340, 200, 360);
+//   pop();
+// }
+
+// /**
+//  * Draws the cat's mouth
+//  */
+// function drawMouth() {
+//   // Cat mouth
+//   push();
+//   strokeWeight(2);
+//   stroke(255, 100, 100);
+//   line(200, 360, 190, 370);
+//   line(200, 360, 210, 370);
+//   pop();
+// }
