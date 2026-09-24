e93db8d45ecf1a35b07727f59d5c9d280320baf9
AgusMaxLee <117362297+AgusMaxLee@users.noreply.github.com>
2026-09-24 17:34:18 -0500

    Added face to the cube

--- topics/instructions/prototype-2/js/script.js
+++ topics/instructions/prototype-2/js/script.js
@@ -1,8 +1,8 @@
 /**
- * Orbiting Shapes
+ * 3D Shapes
  * Agustin Maximo Lee
  *
- * An abstract 3D scene of simple shapes orbiting a centre point.
+ * A 3D box with a simple face on it, rotated by the mouse position.
  */
 
 "use strict";
@@ -15,19 +15,47 @@ function setup() {
 }
 
 /**
- * Draws a single box, rotated based on the mouse position
+ * Draws a single box with a face, rotated based on the mouse position
  */
 function draw() {
     background(20);
 
     // Turn the box based on where the mouse is on the canvas
-    const angleY = map(mouseX, 0, width, 0, TWO_PI);
-    const angleX = map(mouseY, 0, height, 0, TWO_PI);
+    // (angleX is limited to a gentle nod up/down so it never flips upside down)
+    const angleY = map(mouseX, 0, width, -HALF_PI, HALF_PI);
+    const angleX = map(mouseY, 0, height, QUARTER_PI, -QUARTER_PI);
     rotateY(angleY);
     rotateX(angleX);
 
     push();
     fill(200, 100, 255);
     box(100);
+    pop();
+
+    drawFace();
+}
+
+/**
+ * Draws a simple face at the front of the box, so it rotates along with it
+ */
+function drawFace() {
+    // Eyes 
+    push();
+    fill(0);
+    translate(-20, -15, 51);
+    sphere(8);
+    pop();
+
+    push();
+    fill(0);
+    translate(20, -15, 51);
+    sphere(8);
+    pop();
+
+    // Mouth
+    push();
+    fill(200, 30, 30);
+    translate(0, 20, 51);
+    box(40, 8, 5);
     pop();
 }
